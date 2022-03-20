d06e5a6357bf2c90a0d998ee16b40d1625b49a92
Florence Lourdes <99551910+florencelourdes@users.noreply.github.com>
2022-03-20 19:49:54 -0400

    Add files via upload

---
+++ Fish and Fish List/Aquaria/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquaria
+{
+    internal class Fish
+    {
+        String name;
+        public Fish(String name)
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        //Common
+        Fish clownfish = new Fish("clownfish");
+        Fish gobby = new Fish("gobby");
+        Fish puffer = new Fish("puffer");
+        //Uncommon
+        Fish butterfly = new Fish("butterfly");
+        Fish blueTang = new Fish("blueTang");
+        Fish royalGamma = new Fish("royalGamma");
+        //Rare
+        Fish cardinal = new Fish("cardinal");
+        Fish blueDotJawfish = new Fish("blueDotJawfish");
+        //Epic
+        Fish seahorse = new Fish("seahorse");
+        Fish possumWrasse = new Fish("possumWrasse");
+        //Legendary
+        Fish lionfish = new Fish("lionfish");
+        Fish mandarinDragonet = new Fish("mandarinDragonet");
+    }
+}
---
+++ Fish and Fish List/Aquaria/FishList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquaria
+{
+    internal class FishList
+    {
+        List<Fish> fishList;
+        public FishList()
+        {
+            fishList = new List<Fish>();
+        }
+        public void AddFish(Fish fishType)
+        {
+            fishList.Add(fishType);
+        }
+
+        public void RemoveFish(Fish fishType)
+        {
+            fishList.Remove(fishType);
+        }
+    }
+}
