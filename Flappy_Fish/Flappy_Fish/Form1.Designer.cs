8ee2f412261652b8c5ddf9c144d6f9765179957e
Matthew Fisher <matthew.fisher7@snhu.edu>
2022-04-02 13:52:53 -0400

    updated game

--- Flappy_Fish/Flappy_Fish/Form1.Designer.cs
+++ Flappy_Fish/Flappy_Fish/Form1.Designer.cs
@@ -30,18 +30,27 @@ namespace Flappy_Fish
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlappyFish));
             this.score1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Restart = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
             this.Ground = new System.Windows.Forms.PictureBox();
             this.Fish = new System.Windows.Forms.PictureBox();
-            this.PipeBottom = new System.Windows.Forms.PictureBox();
+            this.PipeBottomHigh = new System.Windows.Forms.PictureBox();
             this.PipeTop = new System.Windows.Forms.PictureBox();
+            this.PipeBottom = new System.Windows.Forms.PictureBox();
+            this.PipeTopHigh = new System.Windows.Forms.PictureBox();
+            this.PipeBottomLow = new System.Windows.Forms.PictureBox();
+            this.PipeTopLow = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Fish)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottomHigh)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTopHigh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottomLow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTopLow)).BeginInit();
             this.SuspendLayout();
             // 
             // score1
@@ -49,11 +58,13 @@ namespace Flappy_Fish
             this.score1.AutoSize = true;
             this.score1.BackColor = System.Drawing.Color.Transparent;
             this.score1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score1.ForeColor = System.Drawing.Color.Black;
             this.score1.Location = new System.Drawing.Point(3, 13);
             this.score1.Name = "score1";
             this.score1.Size = new System.Drawing.Size(222, 59);
             this.score1.TabIndex = 4;
             this.score1.Text = "Score: 0 ";
+            this.score1.Click += new System.EventHandler(this.score1_Click);
             // 
             // timer1
             // 
@@ -80,17 +91,18 @@ namespace Flappy_Fish
             this.ExitButton.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ExitButton.Font = new System.Drawing.Font("Rockwell", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ExitButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ExitButton.Location = new System.Drawing.Point(345, 395);
+            this.ExitButton.Location = new System.Drawing.Point(378, 395);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(247, 113);
             this.ExitButton.TabIndex = 6;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = false;
             this.ExitButton.Visible = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // Ground
             // 
-            this.Ground.Image = global::Flappy_Fish.Properties.Resources.ground;
+            this.Ground.Image = ((System.Drawing.Image)(resources.GetObject("Ground.Image")));
             this.Ground.Location = new System.Drawing.Point(-14, 807);
             this.Ground.Name = "Ground";
             this.Ground.Size = new System.Drawing.Size(738, 112);
@@ -100,59 +112,119 @@ namespace Flappy_Fish
             // 
             // Fish
             // 
+            this.Fish.BackColor = System.Drawing.Color.Transparent;
             this.Fish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.Fish.Image = global::Flappy_Fish.Properties.Resources.bird;
-            this.Fish.Location = new System.Drawing.Point(46, 288);
+            this.Fish.Image = ((System.Drawing.Image)(resources.GetObject("Fish.Image")));
+            this.Fish.Location = new System.Drawing.Point(145, 288);
             this.Fish.Name = "Fish";
             this.Fish.Size = new System.Drawing.Size(68, 51);
             this.Fish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Fish.TabIndex = 1;
             this.Fish.TabStop = false;
+            this.Fish.Click += new System.EventHandler(this.Fish_Click);
             // 
-            // PipeBottom
+            // PipeBottomHigh
             // 
-            this.PipeBottom.Image = global::Flappy_Fish.Properties.Resources.pipe;
-            this.PipeBottom.Location = new System.Drawing.Point(563, 543);
-            this.PipeBottom.Name = "PipeBottom";
-            this.PipeBottom.Size = new System.Drawing.Size(118, 364);
-            this.PipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.PipeBottom.TabIndex = 3;
-            this.PipeBottom.TabStop = false;
-            this.PipeBottom.Click += new System.EventHandler(this.GameTimerEvent);
+            this.PipeBottomHigh.Image = ((System.Drawing.Image)(resources.GetObject("PipeBottomHigh.Image")));
+            this.PipeBottomHigh.Location = new System.Drawing.Point(578, 330);
+            this.PipeBottomHigh.Name = "PipeBottomHigh";
+            this.PipeBottomHigh.Size = new System.Drawing.Size(118, 499);
+            this.PipeBottomHigh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeBottomHigh.TabIndex = 3;
+            this.PipeBottomHigh.TabStop = false;
+            this.PipeBottomHigh.Visible = false;
+            this.PipeBottomHigh.Click += new System.EventHandler(this.GameTimerEvent);
             // 
             // PipeTop
             // 
             this.PipeTop.BackColor = System.Drawing.Color.Transparent;
-            this.PipeTop.Image = global::Flappy_Fish.Properties.Resources.pipedown;
-            this.PipeTop.Location = new System.Drawing.Point(563, -14);
+            this.PipeTop.Image = ((System.Drawing.Image)(resources.GetObject("PipeTop.Image")));
+            this.PipeTop.Location = new System.Drawing.Point(578, -14);
             this.PipeTop.Name = "PipeTop";
             this.PipeTop.Size = new System.Drawing.Size(118, 353);
             this.PipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.PipeTop.TabIndex = 0;
             this.PipeTop.TabStop = false;
             // 
+            // PipeBottom
+            // 
+            this.PipeBottom.Image = ((System.Drawing.Image)(resources.GetObject("PipeBottom.Image")));
+            this.PipeBottom.Location = new System.Drawing.Point(578, 543);
+            this.PipeBottom.Name = "PipeBottom";
+            this.PipeBottom.Size = new System.Drawing.Size(118, 480);
+            this.PipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeBottom.TabIndex = 8;
+            this.PipeBottom.TabStop = false;
+            this.PipeBottom.Click += new System.EventHandler(this.PipeBottom_Click);
+            // 
+            // PipeTopHigh
+            // 
+            this.PipeTopHigh.BackColor = System.Drawing.Color.Transparent;
+            this.PipeTopHigh.Image = ((System.Drawing.Image)(resources.GetObject("PipeTopHigh.Image")));
+            this.PipeTopHigh.Location = new System.Drawing.Point(578, -227);
+            this.PipeTopHigh.Name = "PipeTopHigh";
+            this.PipeTopHigh.Size = new System.Drawing.Size(118, 353);
+            this.PipeTopHigh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeTopHigh.TabIndex = 7;
+            this.PipeTopHigh.TabStop = false;
+            this.PipeTopHigh.Visible = false;
+            // 
+            // PipeBottomLow
+            // 
+            this.PipeBottomLow.Image = ((System.Drawing.Image)(resources.GetObject("PipeBottomLow.Image")));
+            this.PipeBottomLow.Location = new System.Drawing.Point(578, 712);
+            this.PipeBottomLow.Name = "PipeBottomLow";
+            this.PipeBottomLow.Size = new System.Drawing.Size(118, 364);
+            this.PipeBottomLow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeBottomLow.TabIndex = 10;
+            this.PipeBottomLow.TabStop = false;
+            this.PipeBottomLow.Visible = false;
+            // 
+            // PipeTopLow
+            // 
+            this.PipeTopLow.BackColor = System.Drawing.Color.Transparent;
+            this.PipeTopLow.Image = ((System.Drawing.Image)(resources.GetObject("PipeTopLow.Image")));
+            this.PipeTopLow.Location = new System.Drawing.Point(578, -14);
+            this.PipeTopLow.Name = "PipeTopLow";
+            this.PipeTopLow.Size = new System.Drawing.Size(118, 522);
+            this.PipeTopLow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeTopLow.TabIndex = 9;
+            this.PipeTopLow.TabStop = false;
+            this.PipeTopLow.Visible = false;
+            // 
             // FlappyFish
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(693, 907);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.PipeTopHigh);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.Restart);
-            this.Controls.Add(this.Ground);
             this.Controls.Add(this.Fish);
             this.Controls.Add(this.score1);
-            this.Controls.Add(this.PipeBottom);
+            this.Controls.Add(this.PipeBottomHigh);
             this.Controls.Add(this.PipeTop);
+            this.Controls.Add(this.PipeBottom);
+            this.Controls.Add(this.PipeTopLow);
+            this.Controls.Add(this.PipeBottomLow);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FlappyFish";
-            this.Text = "   ";
+            this.Text = "Flappy Fish";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FlappyFish_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FlappyFish_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Fish)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottomHigh)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTopHigh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeBottomLow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeTopLow)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,11 +235,15 @@ namespace Flappy_Fish
         private System.Windows.Forms.PictureBox PipeTop;
         private System.Windows.Forms.PictureBox Fish;
         private System.Windows.Forms.PictureBox Ground;
-        private System.Windows.Forms.PictureBox PipeBottom;
+        private System.Windows.Forms.PictureBox PipeBottomHigh;
         private System.Windows.Forms.Label score1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button Restart;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.PictureBox PipeBottom;
+        private System.Windows.Forms.PictureBox PipeTopHigh;
+        private System.Windows.Forms.PictureBox PipeBottomLow;
+        private System.Windows.Forms.PictureBox PipeTopLow;
     }
 }
 
--- Flappy_Fish/Flappy_Fish/Form1.cs
+++ Flappy_Fish/Flappy_Fish/Form1.cs
@@ -15,6 +15,9 @@ namespace Flappy_Fish
         int pipeSpeed = 5; //sets speed at which pipes move
         int gravity = 13;  // makes bird fall
         int score = 0;     //keeps track of how many points you get, may turn to coins
+        Random rnd = new Random();
+        int rand = 0;
+
         public FlappyFish()
         {
             InitializeComponent();
@@ -46,45 +49,144 @@ namespace Flappy_Fish
             score1.Text += " Game over!!!"; //score text box says messge
             Restart.Visible = true; //button to restart game shows
             ExitButton.Visible = true;  //button to exit game shows
-
-
-
         }
         private void GameTimerEvent(object sender, EventArgs e)
         {
             Fish.Top += gravity; //links bird with gravity so it falls/rises
-            PipeBottom.Left -= pipeSpeed; //links pipeSpeed with the pipes so they move
-            PipeTop.Left -= pipeSpeed; //top and bottom
-             score1.Text = "Score: " + score;  //keeps track of score
+            PipeBottom.Left -= pipeSpeed;
+            PipeBottomHigh.Left -= pipeSpeed;
+            PipeBottomLow.Left -= pipeSpeed;//links pipeSpeed with the pipes so they move
+            PipeTop.Left -= pipeSpeed;
+            PipeTopHigh.Left -= pipeSpeed;
+            PipeTopLow.Left -= pipeSpeed;//top and bottom
+            score1.Text = "Score: " + score;  //keeps track of score
 
-            if (PipeBottom.Left < -150)
+            rand = rnd.Next(0, 2);
+
+            if (PipeBottom.Left < -150 || PipeBottomHigh.Left < -150 || PipeBottomLow.Left < -150)
             {
-                PipeBottom.Left = 800;
+                if (rand == 0)
+                {
+                    PipeBottom.Visible = true;
+                    PipeBottom.Left = 800;
+                    PipeBottomHigh.Visible = false;
+                    PipeBottomLow.Visible = false;
+                    PipeBottomHigh.Left = 800;
+                    PipeBottomLow.Left = 800;
+
+                }
+                if (rand == 1)
+                {
+                    PipeBottomHigh.Visible = true;
+                    PipeBottomHigh.Left = 800;
+                    PipeBottom.Visible = false;
+                    PipeBottomLow.Visible = false;
+                    PipeBottom.Left = 800;
+                    PipeBottomLow.Left = 800;
+
+
+                }
+                if (rand == 2)
+                {
+                    PipeBottomLow.Visible = true;
+                    PipeBottomLow.Left = 800;
+                    PipeBottomHigh.Visible = false;
+                    PipeBottom.Visible = false;
+                    PipeBottom.Left = 800;
+                    PipeBottomHigh.Left = 800;
+
+                }
                 score++;   //once the pipes go past the bird they reset and score is added
             }
-            if (PipeTop.Left < -150) //-180
+            if (PipeTop.Left < -150 || PipeTopHigh.Left < -150 || PipeTopLow.Left < -150) //-180
             {
-                PipeTop.Left = 800; //950
-                score++;
-            }
-            if (Fish.Bounds.IntersectsWith(PipeBottom.Bounds) ||
-                Fish.Bounds.IntersectsWith(PipeTop.Bounds) ||
-                Fish.Bounds.IntersectsWith(Ground.Bounds) || Fish.Top < -25
-                )  //if the bird hits the pipes or ground game ends
-            {
-                endGame(); //runs function 
-            }
- 
+                if (rand == 0)
+                {
+                    PipeTop.Visible = true;
+                    PipeTop.Left = 800;
+                    PipeTopHigh.Visible = false;
+                    PipeTopLow.Visible = false;
+                    PipeTopHigh.Left = 800;
+                    PipeTopLow.Left = 800;
 
-           // if (score % 5 == 0 && score > 0) //testing to see if pipes should move faster as you play
-           // {
-           //    pipeSpeed = pipeSpeed + 1;
-           // }
+
+                }
+                if (rand == 1)
+                {
+                    PipeTopHigh.Visible = true;
+                    PipeTopHigh.Left = 800;
+                    PipeTop.Visible = false;
+                    PipeTopLow.Visible = false;
+                    PipeTop.Left = 800;
+                    PipeTopLow.Left = 800;
+                }
+                if (rand == 2)
+                {
+                    PipeTopLow.Visible = true;
+                    PipeTopLow.Left = 800;
+                    PipeTopHigh.Visible = false;
+                    PipeTop.Visible = false;
+                    PipeTopHigh.Left = 800;
+                    PipeTop.Left = 800;
+                }
+            }
+            if (PipeTop.Visible == true)
+            {
+                if (Fish.Bounds.IntersectsWith(PipeBottom.Bounds) ||
+                Fish.Bounds.IntersectsWith(PipeTop.Bounds) ||
+                Fish.Bounds.IntersectsWith(Ground.Bounds) ||
+                Fish.Top < -25)
+                {
+                    endGame();
+                }
+            }
+            if (PipeTopHigh.Visible == true)
+            {
+                if (Fish.Bounds.IntersectsWith(PipeBottomHigh.Bounds) ||
+                Fish.Bounds.IntersectsWith(PipeTopHigh.Bounds) ||
+                Fish.Bounds.IntersectsWith(Ground.Bounds) ||
+                Fish.Top < -25)
+                {
+                    endGame();
+                }
+            }
+            if (PipeTopLow.Visible == true)
+            {
+                if (Fish.Bounds.IntersectsWith(PipeBottomLow.Bounds) ||
+                Fish.Bounds.IntersectsWith(PipeTopLow.Bounds) ||
+                Fish.Bounds.IntersectsWith(Ground.Bounds) ||
+                Fish.Top < -25)
+                {
+                    endGame();
+                }
+            }
+              
+           
         }
 
         private void Restart_Click(object sender, EventArgs e)
         {
             Application.Restart(); //restarts game
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PipeBottom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void score1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
